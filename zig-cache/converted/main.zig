ccf825e7a9ad363af44fcd8ca06aef663d30c55f
Janis Gertulla <j.gertulla@t-online.de>
2021-11-06 20:33:09 +0100

    * Fixe den inf bug

--- zig-cache/converted/main.zig
+++ zig-cache/converted/main.zig
@@ -7,7 +7,7 @@ const c = @import("Constants.zig");
 const print = @import("std").debug.print;
 
 //var particles = p.Particle {.x = undefined, .y = undefined, .z = undefined} ** (c.NUM_PARTICLES);
-var particles = p.Particle {.x = undefined} ** (c.NUM_PARTICLES);
+var particles : [c.NUM_PARTICLES] p.Particle = undefined;
 
 pub fn initParticles() !void {
     var prng = std.rand.DefaultPrng.init(blk: {
@@ -20,11 +20,13 @@ pub fn initParticles() !void {
         while(true) {
             part.x = p.Vec3{.x = rand.float(f32)*@intToFloat(f32, c.BOX_WIDTH), .y = rand.float(f32)*@intToFloat(f32, c.BOX_HEIGHT), .z = rand.float(f32)*@intToFloat(f32, c.BOX_LENGTH) 
                             };
-           // part.v = p.Vec3{.x = 0.0, .y = 0.0, .z }                
+            part.v = p.Vec3{.x = 0.0, .y = 0.0, .z = 0.0};
+            part.f = p.Vec3{.x = 0.0, .y = 0.0, .z = 0.0};
+            part.m = 1.0;
             var valid : bool = true;
-            for (particles) |*part2| {
-                if(part2 == part) break;
+            for (particles[0..idx]) |*part2| {
                 var dist = part.x.distSquare(part2.x);
+                
                 if(dist < 3.1) {
                     valid = false;
                     break;
@@ -38,16 +40,13 @@ pub fn initParticles() !void {
 }
 
 pub fn update(comptime isConservationFrame : bool) void {
-    var p1 : u32 = 0;
-    while (p1 < c.NUM_PARTICLES) : (p1 += 1) {
-        particles[p1].update(isConservationFrame);
-    }
-    p1 = 0;
-    while (p1 < c.NUM_PARTICLES) : (p1 += 1) {
-        var p2 : u32 = 0;
-        while (p2 < p1) : (p2 += 1) {
-            particles[p1].forceBetween(&particles[p2], isConservationFrame);
+    for (particles) |*p1, idx| {
+        for (particles[0..idx]) |*p2| {
+            p1.forceBetween(p2, isConservationFrame);
         }
+    }
+    for (particles) |*p1| {
+        p1.update(isConservationFrame);
     }
 }
 
@@ -63,23 +62,23 @@ pub fn main() !void {
     
     var i : u32 = 0;
     while (i < c.NUM_STEPS) : (i += 1) {
-        if (i % 1 == 0) {
-            _ = try xyz.writer().print("{}\n\n", .{particles.len});
+        if (i % 100 == 0) {
+            _ = try xyz.writer().print("{}\n; Frame {}\n", .{particles.len, i});
             for (particles) |part| {
                 _ = try xyz.writer().print("Ar\t{}\t{}\t{}\n", .{part.x.x, part.x.y, part.x.z});
             }
 
-            print("{}%\n", .{@floatToInt(u32, 100*@intToFloat(f32, i)/@intToFloat(f32, c.NUM_STEPS))});
-            p.currentEnergy = 0;
-            p.currentMomentum = p.Vec3{.x = 0, .y = 0, .z = 0};
-            p.currentAngularMomentum = p.Vec3{.x = 0, .y = 0, .z = 0};
-            update(true);
-            _ = try csv.writer().print("{}, {}, {}, {}, {}, {}, {}, {}, {}, {}\n", 
-                .{@intToFloat(f32, i) * c.DELTA_TIME, 
-                particles[0].x.x, particles[1].x.x, 
-                p.currentEnergy, 
-                p.currentMomentum.x, p.currentMomentum.y, p.currentMomentum.z, 
-                p.currentAngularMomentum.x, p.currentAngularMomentum.y, p.currentAngularMomentum.z});
+            // print("{}%\n", .{@floatToInt(u32, 100*@intToFloat(f32, i)/@intToFloat(f32, c.NUM_STEPS))});
+            // p.currentEnergy = 0;
+            // p.currentMomentum = p.Vec3{.x = 0, .y = 0, .z = 0};
+            // p.currentAngularMomentum = p.Vec3{.x = 0, .y = 0, .z = 0};
+            //  update(true);
+            //  _ = try csv.writer().print("{}, {}, {}, {}, {}, {}, {}, {}, {}, {}\n", 
+            //      .{@intToFloat(f32, i) * c.DELTA_TIME, 
+            //      particles[0].x.x, particles[1].x.x, 
+            //      p.currentEnergy, 
+            //      p.currentMomentum.x, p.currentMomentum.y, p.currentMomentum.z, 
+            //      p.currentAngularMomentum.x, p.currentAngularMomentum.y, p.currentAngularMomentum.z});
         }
         update(false);
     }
