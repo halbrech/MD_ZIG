dda2075a087c8457e3e82b360f5d0d92d7da1c1c
IntegratedQuantum <jahe788@gmail.com>
2021-11-08 21:32:39 +0100

    Fix opengl linking for windows/mac.

--- build.zig
+++ build.zig
@@ -66,7 +66,7 @@ const ConvertStep = struct {
     }
 };
 
-pub fn build(b: *std.build.Builder) void {
+pub fn build(b: *std.build.Builder) !void {
     // Standard target options allows the person running `zig build` to choose
     // what target to build for. Here we do not override the defaults, which
     // means any target is allowed, and the default is native. Other options
@@ -85,7 +85,17 @@ pub fn build(b: *std.build.Builder) void {
     exe.setBuildMode(mode);
     exe.linkSystemLibrary("c");
     exe.linkSystemLibrary("SDL2");
-    exe.linkSystemLibrary("GL");
+    switch (target.getOsTag()) {
+        .windows => exe.linkSystemLibrary("opengl32"),
+        .macos   => exe.linkFramework("OpenGL"),
+        .linux   => exe.linkSystemLibrary("GL"),
+        else => {
+            const triplet = try target.zigTriple(b.allocator);
+            defer b.allocator.free(triplet);
+            std.debug.warn("Unsupported target: {s}\n", .{ triplet });
+            return;
+        }
+    }
     exe.install();
 
     const run_cmd = exe.run();
